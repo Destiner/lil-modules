67f2ea6da386d4d79e276b064bd417260cf26fa6
Timur Badretdinov <accounts@destiner.io>
2024-10-02 18:35:42 +0100

    Remove template contracts

--- script/DeployModule.s.sol
+++ script/DeployModule.s.sol
@@ -5,13 +5,13 @@ import "forge-std/Script.sol";
 import { RegistryDeployer } from "modulekit/deployment/RegistryDeployer.sol";
 
 // Import modules here
-import { ValidatorTemplate } from "src/ValidatorTemplate.sol";
+import { TokenValidator } from "src/TokenValidator/TokenValidator.sol";
 
 /// @title DeployModuleScript
 contract DeployModuleScript is Script, RegistryDeployer {
     function run() public {
         // Setup module bytecode, deploy params, and data
-        bytes memory bytecode = type(ValidatorTemplate).creationCode;
+        bytes memory bytecode = type(TokenValidator).creationCode;
         bytes memory deployParams = "";
         bytes memory data = "";
 
